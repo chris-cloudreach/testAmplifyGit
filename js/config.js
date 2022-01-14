7b7f1c244a8c3dfa52eb9b349166dd6278f2690c
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-01-14 14:33:48 +0000

    UPDATE - Updated config.js file with appclientId and userpoolId

--- js/config.js
+++ js/config.js
@@ -3,9 +3,9 @@
 
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. eu-west-1_FUcVUD99F
-        userPoolClientId: '', // e.g. 4vpt2e23im4qv8jcom9rq66bqm
-        region: '' // eu-west-1
+        userPoolId: 'eu-west-1_0OieCxgWg', // e.g. eu-west-1_FUcVUD99F
+        userPoolClientId: '2654qb3kumu4secigl7m2jmp21', // e.g. 4vpt2e23im4qv8jcom9rq66bqm
+        region: 'eu-west-1' // eu-west-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
