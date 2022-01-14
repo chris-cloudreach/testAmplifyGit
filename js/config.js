7d153a1c6b7070dd29fba5b6bcd9e7b054301a23
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-01-14 15:32:21 +0000

    UPDATE - Integrate Amplify, Cognito user pool, APIGW

--- js/config.js
+++ js/config.js
@@ -8,6 +8,6 @@ window._config = {
         region: 'eu-west-1' // eu-west-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://rd6nt2x0gj.execute-api.eu-west-1.amazonaws.com/DEV' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
