8ec08e5d92ab5d0dd29a92f62e8d15459ad43d60
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-01-14 12:18:12 +0000

    UPDATE - Remove amplify.yml and using inline builspec

--- amplify.tf
+++ amplify.tf
@@ -1,5 +1,5 @@
-resource "aws_amplify_app" "amplify_test3" {
-  name       = "amplify_test3"
+resource "aws_amplify_app" "amplify_test_buildspec" {
+  name       = "amplify_test_buildspec"
   repository = "https://github.com/chris-cloudreach/testAmplifyGit"
 # GitHub personal access token
   access_token = ""
@@ -12,6 +12,27 @@ resource "aws_amplify_app" "amplify_test3" {
 
   #Auto Branch Creation
   enable_auto_branch_creation = true
+  
+  build_spec = <<-EOT
+  version: 1
+  frontend:
+    phases:
+      preBuild:
+        commands:
+          - npm ci
+      # IMPORTANT - Please verify your build commands
+      build:
+        commands: []
+    artifacts:
+      # IMPORTANT - Please verify your build output directory
+      baseDirectory: /
+      files:
+        - '**/*'
+    cache:
+      paths:
+        - node_modules/**/*
+  EOT
+
 
   # The default patterns added by the Amplify Console.
   auto_branch_creation_patterns = [
@@ -26,7 +47,7 @@ resource "aws_amplify_app" "amplify_test3" {
 
 }
 resource "aws_amplify_branch" "AmplifyFeature" {
-  app_id      = aws_amplify_app.amplify_test3.id
+  app_id      = aws_amplify_app.amplify_test_buildspec.id
   branch_name = "main" # must match the branch name in github where all the files are
   stage     = "PRODUCTION"
 }
