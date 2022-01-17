6147f3a72bd148e23f95615f42a33725ba684fee
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-01-17 17:10:38 +0000

    Updating config file in amplify

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-eu-west-1_sh6nZJK7U', // e.g. eu-west-1_FUcVUD99F
+        userPoolId: 'eu-west-1_sh6nZJK7U', // e.g. eu-west-1_FUcVUD99F
         userPoolClientId: '2f510reke5ar9c9rl8eo2vihpc', // e.g. 4vpt2e23im4qv8jcom9rq66bqm
         region: 'eu-west-1' // eu-west-1
     },
